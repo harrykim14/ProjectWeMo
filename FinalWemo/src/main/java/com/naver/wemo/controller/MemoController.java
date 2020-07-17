089d32bdbf875e76fdd86b6400d4d0e8b6ec3be2
harrykim14 <67398691+harrykim14@users.noreply.github.com>
2020-07-17 15:37:12 +0900

    Directly update via GitHub Page not from Eclipse
    7/17 update MemoController

--- FinalWemo/src/main/java/com/naver/wemo/controller/MemoController.java
+++ FinalWemo/src/main/java/com/naver/wemo/controller/MemoController.java
@@ -34,46 +34,44 @@ public class MemoController {
 	
 	@RequestMapping(value = "/Memolist", method = RequestMethod.GET)
 	public ModelAndView getWeMoList(@RequestParam(value = "MEMO_SUB", defaultValue = "STUDY", required = false) String MEMO_SUB, 
-									ModelAndView mv, Memo memo, HttpSession session, HttpServletResponse resp) throws IOException{
+									ModelAndView mv, Memo memo, HttpSession session){
 		String id = (String) session.getAttribute("USER_EMAIL");
-		memo.setUSER_EMAIL(id);
-		memo.setMEMO_SUB(MEMO_SUB);
-		List<Memo> mList = null;
-		if(MEMO_SUB.equals("IMPORTANT"))
-			mList = mService.getFavMemoList(memo);
-		else if(MEMO_SUB.equals("TRASH"))
-			mList = mService.getTraMemoList(memo);
-		else
-			mList = mService.getMemoList(memo);
 		
-		if (mList != null) { // 계정 내에 memo가 존재하면 mList를 모델뷰에 담아 WeMo_Main으로 보냄
-			mv.addObject("Memolist", mList);
-			mv.addObject("MEMO_SUB", MEMO_SUB);
-			mv.setViewName("WeMo_Main");
-			return mv;
-		} else { // 새 계정으로 로그인 하면 mList는 반드시 null이므로 새 메모를 추가
-			if(mService.memoForNewAccount(id)) {
-				//firstInsert를 알아보기 쉽도록 새 계정 메모라고 메소드명만 변경 (return값도 boolean으로 변경)
-			memo.setMEMO_NUM(1);
-			memo = mService.getLatestMemoInfo(memo);
-				// 새 메모를 넣을 때 num값을 1로 고정시켜두고 1번 메모의 상세정보를 가져옴
-			mList = new ArrayList<Memo>(); 
-				// 지역변수 밖에서 조회한 mList값이 null이기 때문에 지역변수 내에서 new로 틀을 새로 정의
-			mList.add(memo);
-				// null인 mList에 memo 객체를 담아 모델뷰에 mList를 담기
-			mv.addObject("Memolist", mList);
-			mv.addObject("MEMO_SUB", MEMO_SUB);
+		mv.addObject("USER_EMAIL", id);
+		mv.addObject("MEMO_SUB", MEMO_SUB);
+		mv.setViewName("WeMo_Main");
+		return mv;
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "getSectionMemoList", method =RequestMethod.POST)
+	public void getSectionMemoList(Memo memoObj, HttpServletResponse resp) {
+		PrintWriter out = null;
+		try {
+			String MEMO_SUB = memoObj.getMEMO_SUB();
+			List<Memo> mList = null;
+			if(MEMO_SUB.equals("IMPORTANT"))
+				mList = mService.getFavMemoList(memoObj);
+			else if(MEMO_SUB.equals("TRASH"))
+				mList = mService.getTraMemoList(memoObj);
+			else
+				mList = mService.getMemoList(memoObj);
 			
-			mv.setViewName("WeMo_Main");
-			return mv;
-			} else { // memoForNewAccount()에서 오류나면 로그인화면으로 history.back()
-				PrintWriter out = resp.getWriter();
-				System.out.println("새로운 계정이 등록되었으나 디폴트 메모를 등록하는데 실패하였습니다.");
-				out.println("<script>alert('오류로 인해 접속할 수 없었습니다. 다시 로그인 해 주세요'); history.back();</script>");
-				out.close();
-				return null;
+			resp.setCharacterEncoding("UTF-8");
+			out = resp.getWriter();
+			if (mList.size() >0) {
+				String jsonMemolist = new Gson().toJson(mList);
+				out.println(jsonMemolist);
+			} else {
+				out.println("false");
 			}
 				
+		} catch(Exception e) { 
+			e.getStackTrace();
+		} finally {
+			if (out != null)
+				out.close();
 		}
 	}
 	
